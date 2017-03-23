ad1d792ed4fdfb94d206207417a0fb66e00069cd
Alexander Robbins <alexi@tamber.com>
2017-03-23 14:50:42 -0700

    Add unsubscribe method

--- update_subscription.go
+++ update_subscription.go
@@ -9,14 +9,53 @@ import (
 	"github.com/dinosaurcop/go-mailchimp/status"
 )
 
+func (c *Client) Unsubscribe(listID, email string) (*MemberResponse, error) {
+	emailMD5 := fmt.Sprintf("%x", md5.Sum([]byte(email)))
+	params := map[string]interface{}{
+		"status": status.Unsubscribed,
+	}
+	resp, err := c.do(
+		"PATCH",
+		fmt.Sprintf("/lists/%s/members/%s", listID, emailMD5),
+		&params,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Allow any success status (2xx)
+	if resp.StatusCode/100 == 2 {
+		// Unmarshal response into MemberResponse struct
+		memberResponse := new(MemberResponse)
+		if err := json.Unmarshal(data, memberResponse); err != nil {
+			return nil, err
+		}
+		return memberResponse, nil
+	}
+
+	// Request failed
+	errorResponse, err := extractError(data)
+	if err != nil {
+		return nil, err
+	}
+	return nil, errorResponse
+}
+
 // UpdateSubscription ...
-func (c *Client) UpdateSubscription(listID, status, email string, mergeFields map[string]interface{}) (*MemberResponse, error) {
+func (c *Client) UpdateSubscription(listID, email string, mergeFields map[string]interface{}) (*MemberResponse, error) {
 	// Hash email
 	emailMD5 := fmt.Sprintf("%x", md5.Sum([]byte(email)))
 	// Make request
 	params := map[string]interface{}{
 		"email_address": email,
-		"status":        status,
+		"status":        status.Subscribed,
 		"merge_fields":  mergeFields,
 	}
 	resp, err := c.do(
