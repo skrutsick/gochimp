751d10b5b2c4196212464fa6db3ce620c15c6da3
Alexander Robbins <alexi@tamber.com>
2017-03-24 00:55:12 -0700

    Unsubscribe

--- client_interface.go
+++ client_interface.go
@@ -9,6 +9,7 @@ type ClientInterface interface {
 	// Exported methods
 	CheckSubscription(listID string, email string) (*MemberResponse, error)
 	Subscribe(listID string, email string, mergeFields map[string]interface{}) (*MemberResponse, error)
+	Unsubscribe(listID, email string) (*MemberResponse, error)
 	UpdateSubscription(listID string, email string, mergeFields map[string]interface{}) (*MemberResponse, error)
 	SetBaseURL(baseURL *url.URL)
 	GetBaseURL() *url.URL
